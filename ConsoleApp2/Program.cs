87dbbf1391da40d5eefadf101b6661680924bc98
Henrique Mendes <henriquemendes13@gmail.com>
2023-08-18 00:03:50 -0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Digite a idade do indivíduo em dias.");
+
+int idade, anos, meses, dias;
+
+idade = int.Parse(Console.ReadLine());
+anos = idade / 365;
+meses = idade % 365/30;
+dias = idade % 365%30;
+
+Console.WriteLine("O indivíduo tem:");
+Console.WriteLine(anos+" ano(s)");
+Console.WriteLine(meses+" mes(es)");
+Console.WriteLine(dias+" dia(s)");
+
